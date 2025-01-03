e12435669b837a8738827ba2c30fb089fcf86c18
Kees Verstoep <c.verstoep@vu.nl>
2025-01-03 20:34:45 +0100

    Properly convert Awari pit state to SVG output

--- pgx/_src/dwg/awari.py
+++ pgx/_src/dwg/awari.py
@@ -1,6 +1,6 @@
 from pgx.awari import State as AwariState
 
-# simple initial version based on 2048 game
+# simple initial version based on version for 2048 game
 
 def _make_awari_dwg(dwg, state: AwariState, config):
     GRID_SIZE = config["GRID_SIZE"]
@@ -19,14 +19,11 @@ def _make_awari_dwg(dwg, state: AwariState, config):
     # board
     # grid
     board_g = dwg.g()
-    for i, _exp2 in enumerate(state._x.board):
-        exp2 = int(_exp2)
-        num = 2**exp2
-        if exp2 > 11:
-            exp2 = 11
+    for i, _num in enumerate(state._x.board):
+        num = int(_num)
         x = (i % 6) * GRID_SIZE
         y = (i // 6) * GRID_SIZE
-        _color = f"{(242-exp2*22):02x}" if color_set.background_color == "white" else f"{(35+exp2*20):02x}"
+        _color = f"{128:02x}"
         board_g.add(
             dwg.rect(
                 (x + 2, y + 2),
@@ -42,14 +39,9 @@ def _make_awari_dwg(dwg, state: AwariState, config):
             )
         )
 
-        if exp2 == 0:
-            continue
-
         font_size = 18
         large_num_color = (
-            f"#{(145+exp2*10):02x}{(145+exp2*10):02x}{(145+exp2*10):02x}"
-            if color_set.background_color == "white"
-            else f"#{(120-exp2*10):02x}{(120-exp2*10):02x}{(120-exp2*10):02x}"
+            f"#{145:02x}{145:02x}{145:02x}"
         )
         board_g.add(
             dwg.text(
@@ -58,7 +50,7 @@ def _make_awari_dwg(dwg, state: AwariState, config):
                     x + GRID_SIZE / 2 - font_size * len(str(num)) * 0.3,
                     y + GRID_SIZE / 2 + 5,
                 ),
-                fill=color_set.text_color if exp2 < 7 else large_num_color,
+                fill=color_set.text_color,
                 font_size=f"{font_size}px",
                 font_family="Courier",
                 font_weight="bold",
